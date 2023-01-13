0f429bee8dced2fcb2e3ac0c465ffd772a948eb3
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-01-13 13:16:17 +0300

    Added operators lesson

---
+++ 05_Operators/Operators/main.swift
@@ -0,0 +1,67 @@
+//
+//  main.swift
+//  Operators
+//
+//  Created by Enes on 13.01.2023.
+//
+
+import Foundation
+
+//Unary Operatorler: Tek değerle kullanılan operatörler (Örn: değil (!x) operatörü)
+//Binary Operatörler: İki değerle kullanılan operatörler (Örn: toplama(x + y) operatörü)
+//Ternary Operatörler: Üç değerle kullanılan operatörler (Örn: koşul(x ? y : z) operatörü)
+
+//MARK: - Atama Operatörü(=)
+
+//let x = 10
+//var y = 5
+//y = x
+//print(y)
+
+//MARK: - Aritmetik Operatörler(+, -, *, /, %)
+
+//print(3 + 2)
+
+//print(5 - 1)
+
+//print(9 * 7)
+
+//print(8 / 2)
+
+//Mod operatör kalan değerini verir.
+//print(15 % 5)
+
+//print(7 % 3)
+
+//print("Kodlu" + "yoruz")
+
+//MARK: -Bileşik atama operatörleri(+=, -=, *=, /=)
+
+//var x = 3
+//x += 2
+//x = x + 2
+//print(x)
+
+//MARK: -Karşılaştırma Operatörleri(==, !=, <, >, >=, <=)
+
+//print(7 == 7)
+
+//print(5 != 2)
+
+//print(9 > 6)
+
+//print(7 < 3)
+
+//print(8 >= 8)
+
+//print(5 <= 1)
+
+if "x" == "x" {
+    print("Eşitlik Doğrudur.")
+} else {
+    print("Eşitlik yanlış.")
+}
+
+//MARK: - Koşul Operatörü(x ? y : z)
+
+//"x" == "x" ? print("Eşitlik Doğrudur.") : print("Eşitlik Yanlış.")
